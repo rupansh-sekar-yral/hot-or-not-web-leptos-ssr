75c55baf1f73a2a42755d959254bc8a07b547b1a
Harsha <106158714+harsh-mn-yral@users.noreply.github.com>
2025-05-29 17:57:00 +0530

    Fix icpump popup text (#868)
    * fix popup
    
    * update

--- ssr/src/component/src/popup.rs
+++ ssr/src/component/src/popup.rs
@@ -6,7 +6,7 @@ use leptos::prelude::*;
 pub fn Popup(#[prop(into)] show: RwSignal<bool>, children: ChildrenFn) -> impl IntoView {
     view! {
         <ShadowOverlay show>
-            <div style="min-height: 500px" class="mx-4 py-4 px-[20px] max-w-full relative max-h-full items-center gap-5 cursor-auto flex-col flex justify-between bg-neutral-900 rounded-md">
+            <div style="min-height: 500px; max-width:40rem;" class="mx-auto py-4 px-[20px] relative max-h-full items-center gap-5 cursor-auto flex-col flex justify-between bg-neutral-900 rounded-md">
                 <div class="pb-4 w-full flex-1">{children()}</div>
                 <div class="flex justify-center w-full items-center px-8">
                     <HighlightedButton
--- ssr/src/page/src/token/icpump_sunset_popup.rs
+++ ssr/src/page/src/token/icpump_sunset_popup.rs
@@ -9,12 +9,12 @@ pub fn IcpumpSunsetPopup() -> impl IntoView {
     view! {
         <Popup show={icpump_sunset_popup_ctx.show}>
             <div class="flex flex-col gap-4 text-center">
-                <h1 style="font-size: 3.5rem" class="font-bold pt-24">"👋"</h1>
+                <h1 style="font-size: 3.5rem" class="font-bold pt-16">"👋"</h1>
                 <div class="text-3xl font-semibold text-neutral-100">
-                    "ICPum.fun is being sunset soon."
+                    "ICPump.fun is being sunset soon."
                 </div>
                 <div class="text-neutral-200 text-xl">
-                    "All created tokens will be dissolved. In case of any concerns, <br/>please reach out to us on " <a href="https://t.me/HotOrNot_app" target="_blank" class="underline">Telegram</a>
+                    "All created tokens will be dissolved. In case of any concerns, "<br/>"please reach out to us on " <a href="https://t.me/HotOrNot_app" target="_blank" class="underline">Telegram</a>
                 </div>
             </div>
         </Popup>
