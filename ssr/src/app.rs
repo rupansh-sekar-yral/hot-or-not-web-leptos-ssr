103049b74c52f3db49cb5f3a4eb633899a24c975
Prakash <prakash@gobazzinga.io>
2025-07-04 19:35:23 +0530

    Added facebook-domain-verification meta tag (#977)

--- ssr/src/app.rs
+++ ssr/src/app.rs
@@ -64,6 +64,7 @@ pub fn shell(options: LeptosOptions) -> impl IntoView {
             <head>
                 <meta charset="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="facebook-domain-verification" content="sqtv2sr90ar0ck7t7zcklos44fw8t3" />
                 <script fetchpriority="low" type="module" src="/js/sentry-init.js" async></script>
                 <script fetchpriority="low" type="module" src="/js/mixpanel-init.js" async></script>
                 <script
